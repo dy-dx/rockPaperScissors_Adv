a0c00370503b15b978ee304ec59b9dfb542e2dba
Chris Kim <christopherjkim@gmail.com>
2015-10-21 14:50:32 -0400

    standard solution

--- starter_code/script.js
+++ starter_code/script.js
@@ -4,17 +4,35 @@ var aiPoint = 0;
 // This function returns the selection of the computer
 function getAISelection() {
     //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    var options = ['rock', 'paper', 'scissors'];
+    return options[Math.floor(Math.random()*3)];
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
     //TODO: pick the correct winner: user or ai
     //TODO: Add one point for the winner
+    var counters = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+    };
+
+    if (userValue === aiValue) {
+        return 'draw';
+    } else if (counters[userValue] === aiValue) {
+        userPoint++;
+        return 'user';
+    } else if (counters[aiValue] === userValue) {
+        aiPoint++;
+        return 'ai';
+    }
 }
 
 // This function sets the scoreboard with the correct points
 function setScore() {
-
+    $('#userPoint').text(userPoint);
+    $('#aiPoint').text(aiPoint);
 }
 
 // This function captures the click and picks the winner
@@ -23,6 +41,7 @@ function evaluate(evt) {
     var aiValue = getAISelection();
 
     var winner = pickWinner(userValue, aiValue);
+    setScore();
 
     if ( 'user' === winner ) {
         $('#message').delay(50).text('You have won!, Click a box to play again');
@@ -35,5 +54,6 @@ function evaluate(evt) {
 
 // This function runs on page load
 $(document).ready(function(){
-
+    setScore();
+    $('.token').click(evaluate);
 });
